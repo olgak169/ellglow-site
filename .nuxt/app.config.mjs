b586af8e6c73c90ffbd8b9a1a9ab3cb93bd830e8
olgak169 <okrupnova@gmail.com>
2023-07-17 13:12:56 +0600

    cleanup: remove files

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/olgakrupnova/Documents/git-folders/ellglow-site/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
